efd8b14f2713f3618e598175579f705b58635b3f
Solenya <antoinemarc.auffray@gmail.com>
2021-02-16 11:04:40 +0100

    Adding first draft of the display part of the interface

---
+++ graphic_interface/display_app_main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from functools import partial
+import functions
+import display_app
+
+defense_folder = "./record/goal/"
+attack_folder = "./record/attack_seq/"
+analysis_folder = "./analysis/"
+
+wh = 600
+ww = 900
+
+dis_app = display_app.Display_App(defense_folder, attack_folder, analysis_folder, wh, ww)
+
+dis_app.run()
